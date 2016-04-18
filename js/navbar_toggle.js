5c32053b18269d23d9c61cff5ec673596a851789
Tony Abboud <tdabboud@hotmail.com>
2016-04-18 00:22:13 -0400

    initial website

---
+++ js/navbar_toggle.js
@@ -0,0 +1,21 @@
+// Show the navbar when the user scrolls past the page description
+(function ($) {
+  $(document).ready(function(){
+    
+    // hide .navbar first
+    $(".navbar").hide();
+    
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+            // Only start the fade in once we have reached the top of the page-description
+            if ($(this).scrollTop() > $('.page-title').offset().top) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+});
+  }(jQuery));
+
